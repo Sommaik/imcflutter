f3cc8d503bc982182824f91a0796fea67507bf28
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-10-17 14:23:56 +0700

    add app bloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:imcflutter/counter/bloc/bloc.dart';
 import 'package:imcflutter/counter/ui/ui.dart';
+import 'package:imcflutter/shared/bloc/bloc.dart';
 import 'package:imcflutter/shared/my_bloc_delegate.dart';
 
 void main() {
   BlocSupervisor.delegate = MyBlocDelegate();
-  runApp(MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          builder: (context) => AppBloc(),
+        )
+      ],
+      child: BlocBuilder<AppBloc, AppState>(
+        builder: (context, state) {
+          if (state is UnAuthorizedState) {
+            return UnAuthorizeApp();
+          } else if (state is AuthorizedState) {
+            return MyApp();
+          } else {
+            return LoadingApp();
+          }
+        },
+      ),
+    ),
+  );
+}
+
+class LoadingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+class UnAuthorizeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
 }
 
 class MyApp extends StatelessWidget {
