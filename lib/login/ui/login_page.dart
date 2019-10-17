c603627eb76c8ccf4143affd088ef7a0143f7280
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-10-17 16:36:40 +0700

    add login form

--- lib/login/ui/login_page.dart
+++ lib/login/ui/login_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Image.asset('images/angular-logo.png'),
+              buildEmailField(),
+              buildPasswordField(),
+              RaisedButton(
+                child: Text("Login"),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text("Register new account"),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  TextFormField buildPasswordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Please fill in your password.',
+        prefixIcon: Icon(Icons.lock),
+      ),
+      obscureText: true,
+    );
+  }
+
+  TextFormField buildEmailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'Please fill in email address',
+        prefixIcon: Icon(Icons.email),
+      ),
+      keyboardType: TextInputType.emailAddress,
     );
   }
 }
