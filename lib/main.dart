84f60921baeae6f6cc29248467a7548c13bce618
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-10-17 08:49:30 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyFullCounterPage(),
+    );
+  }
+}
+
+class MyCounterPage extends StatelessWidget {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App.... '),
+      ),
+      body: Center(
+        child: Text(
+          'Your number is $counter',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.cyan,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          counter += 1;
+          print('numer = $counter');
+        },
+      ),
+    );
+  }
+}
+
+class MyFullCounterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyFullCounterState();
+  }
+}
+
+class MyFullCounterState extends State<MyFullCounterPage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App.... '),
+      ),
+      body: Center(
+        child: Text(
+          'Your number is $counter',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.cyan,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+          print('numer = $counter');
+        },
+      ),
+    );
+    ;
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
